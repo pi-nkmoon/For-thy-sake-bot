fbebaf62966c98fad653ba7218d888dd93975fe5
pi-nkmoon <102548224+pi-nkmoon@users.noreply.github.com>
2026-01-20 18:30:22 +0530

    Replace API keys with placeholder values
    Updated API keys in config.py to placeholder values.

--- masala/config.py
+++ masala/config.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Source account API keys (the account you're copying from)
-SOURCE_API_KEY = 'VLDU11Ftu8KLTb8Jk6y3AFKG7H9d2LMqgftdfUy2yFzmQdh0FHnqZeJJhpbFJDlP'
-SOURCE_API_SECRET = 'TWLaKmCH4uquokYAraroV6lhyDaDfvBRhfffc7B0ScsiUnx1g4of1z8qCs5bNGkk'
+SOURCE_API_KEY = 'source api key'
+SOURCE_API_SECRET = 'source api secret key'
 
 # Target account API keys (the account you're copying to)
-TARGET_API_KEY = 'Hi1ZpW3oBNH991nA8As1hCxxetiLHB1p9aFiJkboA6V4eCJ9Eb2bnZpJhU3GFax8'
-TARGET_API_SECRET = 'COW1SfTxvWOHchhb4o8ASSO5x07QVlzTIPjA08WYwyPumr2qDqjUv8KS39UDnBsr'
+TARGET_API_KEY = 'Target api key'
+TARGET_API_SECRET = 'Target api secret key'
 
 # Trading pair
 TRADING_PAIR = 'ETHUSDT'
