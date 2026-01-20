fdebce7f605968c2defb4eab9594c741b3c8248f
pi-nkmoon <102548224+pi-nkmoon@users.noreply.github.com>
2026-01-20 18:53:29 +0530

    Updated keys
    ..

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE_URI = 'sqlite://///Users/mihirchaturvedi/for_thy_sake_bot/trading_monitor/data/trades.db'
 
-API_KEY = 'VLDU11Ftu8KLTb8Jk6y3AFKG7H9d2LMqgftdfUy2yFzmQdh0FHnqZeJJhpbFJDlP'
-API_SECRET ='TWLaKmCH4uquokYAraroV6lhyDaDfvBRhfffc7B0ScsiUnx1g4of1z8qCs5bNGkk'
+API_KEY = 'api key'
+API_SECRET ='api secret key'
 
 
 SYMBOLS = ['BTCUSDT']  
